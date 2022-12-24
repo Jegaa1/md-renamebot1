cfc596cdc164850449e62ac001c7aac32a732aaf
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2022-12-24 23:48:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 class Config(object):
     BANNED_USERS = []
     DOWNLOAD_LOCATION = "./DOWNLOADS" 
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH" "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
-    AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", None)
-    DATABASE_URI = os.environ.get("DATABASE_URI", None)
+    API_ID = int(os.environ.get("API_ID", 1923471))
+    API_HASH = os.environ.get("API_HASH", "fcdc178451cd234e63faefd38895c991")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5424505458:AAHMuDGeW37kqaipsJAZbCEsJF8BM-4tMco")
+    OWNER_ID = int(os.environ.get("OWNER_ID", 880087645))
+    AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "JAsuranserials")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Erichdaniken:Erichdaniken@cluster0.vhu3d.mongodb.net/?retryWrites=true&w=majority")
