9a74b6f96f071cc46d403a306379b812721416a1
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2022-12-26 22:51:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "fcdc178451cd234e63faefd38895c991")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5424505458:AAGIwqM1MMKB5TToJjaj9ecf25_YSA3ZVNQ")
     OWNER_ID = int(os.environ.get("OWNER_ID", 880087645))
-    AUTH_CHANNEL = "-1001695270196"
+    AUTH_CHANNEL = -1001695270196
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Erichdaniken:Erichdaniken@cluster0.vhu3d.mongodb.net/?retryWrites=true&w=majority")
